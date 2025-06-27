d1108f57077aaa0d32bad6abf6104f7627dfa10e
Joshua Ackerly <ackerlyj05@gmail.com>
2025-06-27 02:48:43 -0400

    feat: enhance Music and Newsletter sections

--- resources/js/pages/Music.tsx
+++ resources/js/pages/Music.tsx
@@ -8,7 +8,7 @@ const Music: React.FC = () => {
             <Header />
             <main className="flex flex-col items-center justify-center bg-[#111111] p-4 text-center text-white">
                 <div>
-                    <ul className="grid grid-cols-2">
+                    <ul className="grid grid-cols-2 md:m-auto md:w-1/2 md:gap-4">
                         <li className="col-span-2 flex w-full justify-center sm:col-span-1">
                             <img
                                 src="/images/RetroVinylRecordArtPsychedelicGraphicDesignforFunkyAlbumCovers.webp"
@@ -41,34 +41,43 @@ const Music: React.FC = () => {
                 </div>
                 <div>
                     <div>
-                        <h2 className="text-3xl text-white">On Tour</h2>
                         <div className="relative max-w-full">
-                            <div className="absolute inset-0">
-                                <img
-                                    src="/images/TourBusInteriorwithInstrumentsandStageEquipment copy.webp"
-                                    loading="lazy"
-                                    alt="#"
-                                    className="pointer-events-none h-full w-full object-cover opacity-80"
-                                />
-                            </div>
                             <div>
-                                <p className="relative z-10 mt-4 max-w-3xl text-lg leading-relaxed font-normal text-white">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur, nisi
-                                    nisl aliquam enim, nec dictum urna elit nec urna. Mauris non erat vitae enim cursus dictum. Etiam euismod, justo
-                                    at facilisis cursus, urna erat laoreet enim, nec dictum urna elit nec urna. Suspendisse potenti. Sed euismod, urna
-                                    eu tincidunt consectetur, nisi nisl aliquam enim, nec dictum urna elit nec urna. Mauris non erat vitae enim cursus
-                                    dictum. Etiam euismod, justo at facilisis cursus, urna erat laoreet enim, nec dictum urna elit nec urna.
-                                    Suspendisse potenti. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, nec dictum urna elit nec
-                                    urna. Mauris non erat vitae enim cursus dictum. Etiam euismod, justo at facilisis cursus, urna erat laoreet enim,
-                                    nec dictum urna elit nec urna. Suspendisse potenti. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam
-                                    enim, nec dictum urna elit nec urna. Mauris non erat vitae enim cursus dictum. Etiam euismod, justo at facilisis
-                                    cursus, urna erat laoreet enim, nec dictum urna elit nec urna. Suspendisse potenti.
-                                </p>
-                                <img
-                                    src="/images/Mandreamsofahousebytheseaatsunsetsurrealartforalbumcoverposterorbookillustration.webp"
-                                    alt="#"
-                                    className="relative z-10 mx-auto p-6 opacity-80"
-                                />
+                                <h2 className="relative z-10 text-3xl text-white">On Tour</h2>
+                                <div className="absolute inset-0">
+                                    <img
+                                        src="/images/TourBusInteriorwithInstrumentsandStageEquipment copy.webp"
+                                        loading="lazy"
+                                        alt="#"
+                                        className="pointer-events-none h-full w-full object-cover opacity-80"
+                                    />
+                                </div>
+                            </div>
+                            <div className="relative flex items-center justify-center">
+                                <div className="relative flex flex-col items-center justify-center p-6 md:flex-row md:items-start">
+                                    <div className="relative z-10 m-auto text-lg font-normal text-white md:w-full md:p-10">
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur,
+                                        nisi nisl aliquam enim, nec dictum urna elit nec urna. Mauris non erat vitae enim cursus dictum. Etiam
+                                        euismod, justo at facilisis cursus, urna erat laoreet enim, nec dictum urna elit nec urna. Suspendisse
+                                        potenti. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, nec dictum urna elit nec urna.
+                                        Mauris non erat vitae enim cursus dictum.
+                                        <div className="">
+                                            {' '}
+                                            Etiam euismod, justo at facilisis cursus, urna erat laoreet enim, nec dictum urna elit nec urna.
+                                            Suspendisse potenti. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, nec dictum urna
+                                            elit nec urna. Mauris non erat vitae enim cursus dictum. Etiam euismod, justo at facilisis cursus, urna
+                                            erat laoreet enim, nec dictum urna elit nec urna. Suspendisse potenti. Sed euismod, urna eu tincidunt
+                                            consectetur, nisi nisl aliquam enim, nec dictum urna elit nec urna. Mauris non erat vitae enim cursus
+                                            dictum. Etiam euismod, justo at facilisis cursus, urna erat laoreet enim, nec dictum urna elit nec urna.
+                                            Suspendisse potenti.
+                                        </div>
+                                    </div>
+                                    <img
+                                        src="/images/Mandreamsofahousebytheseaatsunsetsurrealartforalbumcoverposterorbookillustration.webp"
+                                        alt="#"
+                                        className="relative z-10 mx-auto my-auto flex p-6 md:w-1/3"
+                                    />
+                                </div>
                             </div>
                         </div>
                     </div>

2a8193e3bc5886549868899888440554d52aa702
Joshua Ackerly <ackerlyj05@gmail.com>
2025-06-27 02:48:43 -0400

    Changes to Music and Newsletter

--- resources/js/components/Newsletter.tsx
+++ resources/js/components/Newsletter.tsx
@@ -5,7 +5,7 @@ const Newsletter: React.FC = () => (
         {/* Content */}
 
         <div className="max-w-lg rounded-2xl bg-[#333333] p-2 text-white">
-            <h3 className="text-center text-4xl font-bold text-[#6633ff] md:text-4xl">Newsletter Signup</h3>
+            <h3 className="relative text-center text-4xl font-bold text-[#6633ff] md:text-4xl">Newsletter Signup</h3>
             {/* Optional form can be added here */}
         </div>
     </div>
